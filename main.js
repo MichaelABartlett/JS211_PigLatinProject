ca2344bb8798e9e0ea392f4d1a318c3d319e2147
MichaelABartlett <michael@machinistchoice.com>
2021-07-03 20:32:19 -0500

    finished working program for the first time

--- main.js
+++ main.js
@@ -10,12 +10,83 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+// ************************** my code below ******************************
 
-const pigLatin = (word) => {
+/**
+ * return the position of the first vowel in the word
+ * in no vowel is in the word, return -1
+ * @param {string} word
+ */
+ //let aWord = "swm"
 
-  // Your code here
+ function positionOfFirstVowel(word){
+  // try array of vowels in a loop using indexOf method
+
+  // go thru every letter in the word
+  // and if I find a vowel, i will return
+  // the position of the letter right away
+  // without going thru the rest of the loop
+  for(let i=0; i < word.length; i++){
+    let letter = word[i];
+    // if("aeiou".includes(letter)){
+    if(letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u"){
+    
+      return i;
+    }
+  }
+
+   return -1;
 
 }
+
+
+
+const pigLatin = (word) => {
+  let aWord = word.toLowerCase().trim();
+  positionOfFirstVowel(aWord);
+  //console.log("first vowel:", positionOfFirstVowel(aWord));
+  if(positionOfFirstVowel(aWord) === -1){ 
+    let noVowel = aWord.concat("ay");
+    //console.log("no vowel in word:" + noVowel);
+    return noVowel;
+  } else if(positionOfFirstVowel(aWord) === 0) {
+      let firstVowel = aWord.concat("yay")
+      //console.log("word has vowel first:" + firstVowel);
+      return firstVowel;
+    } else {
+      
+      //console.log("this is aWord:" + aWord)
+      //console.log("first vowel:", positionOfFirstVowel(aWord));
+      //console.log("aWord slice:" + aWord.slice(positionOfFirstVowel(aWord)));
+      let cutWordEnd = aWord.slice(positionOfFirstVowel(aWord));
+      //console.log("cutWordEnd:" + cutWordEnd);
+      let cutWordStart = aWord.substring(positionOfFirstVowel(aWord),-1);
+      //console.log("cutWordStart:" + cutWordStart);
+      let cWord = cutWordEnd.concat(cutWordStart).concat("ay")
+      //console.log("cWord: " + cWord)
+      return cWord;
+    }
+    //console.log("still working on it")
+  
+}
+//pigWork(aWord);
+
+//  let cutCWord = cWord.slice(0,1);
+// let newWord = cWord.slice(1).concat(cutCWord).concat("ay"); 
+
+
+
+
+
+
+
+  
+  // Your code here
+
+
+
+// ******************************* my code above *********************************************
+
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
